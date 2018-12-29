09424f3a0477ddebbe34a8976ed2109e211eca58
Dreamwalker <aristojeff@gmail.com>
2018-12-29 13:52:27 +0900

    a

--- lib/home.dart
+++ lib/home.dart
@@ -44,12 +44,53 @@ class _HomePageState extends State<HomePage> {
               currentAccountPicture: GestureDetector(
                 child: CircleAvatar(
                   backgroundColor: Colors.grey,
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
                 ),
-
               ),
               accountName: Text("Dreamwalker"),
               accountEmail: Text("aristojeff@gmail.com"),
+              decoration: BoxDecoration(
+                color: Colors.pink,
+              ),
             ),
+
+            //Body
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Home Page"),
+                leading: Icon(Icons.home),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("My Account"),
+                leading: Icon(Icons.person),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("My Orders"),
+                leading: Icon(Icons.shopping_basket),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Categories"),
+                leading: Icon(Icons.dashboard),
+              ),
+            ),
+
           ],
         ),
       ),
