52bbc8f60aad2f3b9ac6ba05243a1d7496ffafc5
Dreamwalker <aristojeff@gmail.com>
2018-12-31 13:51:17 +0900

    ff

--- lib/components/horizontal_listview.dart
+++ lib/components/horizontal_listview.dart
@@ -4,14 +4,40 @@ class TopHorizontalList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 80.0,
+      height: 120.0,
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: <Widget>[
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
+          Category(
+            image_location: "assets/cats/dress.png",
+            image_caption: "Dress",
+          ),
+          Category(
+            image_location: "assets/cats/accessories.png",
+            image_caption: "accessories",
+          ),
+          Category(
+            image_location: "assets/cats/formal.png",
+            image_caption: "Formal",
+          ),
+          Category(
+            image_location: "assets/cats/informal.png",
+            image_caption: "Informal",
+          ),
+          Category(
+            image_location: "assets/cats/jeans.png",
+            image_caption: "Jeans",
+          ),
+
+          Category(
+            image_location: "assets/cats/shoe.png",
+            image_caption: "Shoe",
+          ),
+          Category(
+            image_location: "assets/cats/tshirt.png",
+            image_caption: "Tshirt",
+          ),
+
         ],
       ),
     );
@@ -22,10 +48,32 @@ class Category extends StatelessWidget {
   final String image_location;
   final String image_caption;
 
-  Category(this.image_location, this.image_caption);
+  Category({this.image_location, this.image_caption});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          width: 100.0,
+          height: 100.0,
+          child: ListTile(
+              title: Image.asset(
+                image_location,
+                width: 100.0,
+                height: 100.0,
+              ),
+              subtitle: Container(
+                alignment: Alignment.topCenter,
+                child: Text(
+                  image_caption,
+                  style: TextStyle(color: Colors.black, fontSize: 12.0),
+                ),
+              )),
+        ),
+      ),
+    );
   }
 }
